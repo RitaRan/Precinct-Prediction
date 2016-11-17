37e9d7914b100289008f572471fd36ac82dd3a8d
Ting Dai <td104@saxon.stat.duke.edu>
2016-11-17 12:17:29 -0500

    Some write up

--- merge.R
+++ merge.R
@@ -8,18 +8,20 @@ load("/data/nyc_parking/NYParkingViolations.Rdata")
 
 ## Get pluto data
 
+# Gain the address and geometry, the longtitude and latitude
 pluto = st_read("/data/nyc_parking/pluto_manhattan/MNMapPLUTO.shp") %>%
   select(Address, geometry)
-
+## unlist the geometry data in the pluto table, and gain the longtitude and latitude seperately
 pluto_xy = cbind(
   select(pluto, Address),
   st_centroid(pluto) %>% 
     unlist() %>% 
     matrix(ncol=2,byrow=TRUE)
 ) %>% 
-  setNames(c("address","x","y")) %>%
+  setNames(c("address","x","y")) %>% ## asgin x to longtitude, y to latitude
   tbl_df()
 
+## 
 ggplot(pluto_xy, aes(x=x,y=y)) + 
   geom_point(alpha=0.1,size=0.1) +
   theme_bw()
@@ -27,8 +29,10 @@ ggplot(pluto_xy, aes(x=x,y=y)) +
 
 ## Merge data
 
+## Provide the precincts in Manhattan
 valid_precincts = c(1, 5, 6, 7, 9, 10, 13, 14, 17, 18, 19, 20, 22, 23, 24, 25, 26, 28, 30, 32, 33, 34)
 
+## Gain the data of address and precients in the Manhattan
 nyc_man = nyc %>%
   mutate(address = paste(House.Number, Street.Name)) %>%
   filter(Violation.Precinct %in% valid_precincts) %>%
@@ -75,7 +79,7 @@ pluto_xy %<>% mutate(address = tolower(address))%>%
   mutate(address = str_replace(address," (boulevard|bl$)"," blvd"))%>%
   mutate(address = str_replace(address," plce"," place"))
 
-
+## inner combine the both data sets based on address
 combined = inner_join(nyc_man, pluto_xy)
 #not_man = anti_join(nyc_man,pluto_xy)
 #not_pluto = anti_join(pluto_xy, nyc_man)
